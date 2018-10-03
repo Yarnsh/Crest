85a45ca34ebbc3ef651863a4882971a23df358eb
DESKTOP-NCAH18A\Yan <yandshnayder@gmail.com>
2018-10-03 19:57:21 -0400

    Basic state methods for maps

--- Main/Maps/Map.gd
+++ Main/Maps/Map.gd
@@ -3,3 +3,31 @@ extends Navigation
 onready var Players = $Players
 onready var NPCs = $NPCs
 onready var Items = $Items
+
+func _load_item(state):
+	var item = load("res://Items/" + state["type"] + ".tscn").instance()
+	Items.add_child(item)
+	item.setState(state)
+	item.set_name(item.id)
+	return item
+
+func getState():
+	state = {}
+	state["Items"] = {}
+	for item in Items.get_children():
+		state["Items"][item.name] = item.getState()
+	return state
+func setState(state):
+	var items = state["Items"]
+	for i in Items.children():
+		var id = i.name
+		if (items.has(id)):
+			if (items[id] != null):
+				pass
+				i.setState(items[id])
+		else:
+			pass
+			_remove_item(id)
+		items.erase(id)
+	for key in items:
+		_load_item(items[key])
