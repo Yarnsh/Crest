8a50b0329958fbcc2b8519d67c5f9582aa2a68e5
DESKTOP-NCAH18A\Yan <yandshnayder@gmail.com>
2018-10-05 22:16:32 -0400

    Placeholder popup on death

--- Controllers/PController.gd
+++ Controllers/PController.gd
@@ -22,6 +22,7 @@ onready var inventoryUI = $CanvasLayer/Inventory
 onready var abilityUI = $CanvasLayer/Abilities
 onready var HUD = $CanvasLayer/HUD
 onready var damageUI = $CanvasLayer/Damage
+onready var respawnUI = $CanvasLayer/Respawn
 onready var WalkMarker = $WalkMarker
 
 var attacking = false 
@@ -108,6 +109,10 @@ func _on_Actor_equipment_updated():
 
 func _on_Actor_damage_updated():
 	damageUI.updateDamage()
+	if (actor.is_dead):
+		respawnUI.show()
+	else:
+		respawnUI.hide()
 
 func _process(delta):
 	OS.set_window_title(title + " | fps: " + str(Engine.get_frames_per_second()))
