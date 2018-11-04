25fcd86b71e7302455c11f2d8c7cfc4a63b1f4dd
DESKTOP-NCAH18A\Yan <yandshnayder@gmail.com>
2018-11-04 14:13:08 -0500

    Make bow use ray attack

--- Abilities/AbilityIndicatorRay.gd
+++ Abilities/AbilityIndicatorRay.gd
@@ -1,14 +1,14 @@
 extends ImmediateGeometry
 
 func _process(delta):
-	var pos = get_parent().get_global_position()
+	var pos = get_parent().get_parent().get_global_position()
 	
-	var dir = get_parent().cast_to.normalized()
+	var dir = get_parent().get_parent().cast_to.normalized()
 	var side = dir.tangent()
-	var dist = get_parent().cast_to.length()
+	var dist = get_parent().get_parent().cast_to.length()
 	
-	if (get_parent().is_colliding()):
-		dist = (get_parent().get_collision_point() - pos).length()
+	if (get_parent().get_parent().is_colliding()):
+		dist = (get_parent().get_parent().get_collision_point() - pos).length()
 	
 	var p = []
 	p.append(side * 5.0)
@@ -24,3 +24,8 @@ func _process(delta):
 	end()
 	
 	transform.origin = global.to3D(pos)
+	var look = get_parent().get_parent().get_parent().getPointTowards()
+	var look3D = global.to3D(look)
+	if (look3D.x != pos.x and look3D.z != pos.y):
+		look_at(look3D, Vector3(0,1,0))
+		rotate_y(PI/2.0)
